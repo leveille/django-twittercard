40fb31bd89c73df791005460f580b2d5e6c3e709
Jason Leveille <jason@leve.us>
2013-01-26 20:52:14 -0500

    Some pep8 cleanup.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-twittercard",
-    version = "0.0.1",
-    description = "Provides something here...",
+    name="django-twittercard",
+    version="0.0.1",
+    description="Provides something here...",
     long_description=open('README.markdown').read(),
-    author = "Jason Leveille",
-    author_email = "jason@leve.us",
-    url = "https://github.com/leveille/django-twittercard",
+    author="Jason Leveille",
+    author_email="jason@leve.us",
+    url="https://github.com/leveille/django-twittercard",
     license='BSD',
     classifiers=[
         'Development Status :: 4 - Beta',
